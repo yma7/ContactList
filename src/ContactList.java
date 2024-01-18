ab8eaab673a8021278cb02a7fd144589a6e9b74a
yuanyema <yuanye.ma@menloschool.org>
2024-01-18 13:31:55 -0800

    Finished

--- src/ContactList.java
+++ src/ContactList.java
@@ -6,7 +6,7 @@ public class ContactList {
 
     public ContactList()
     {
-        ArrayList<Person> contacts = new ArrayList<Person>();
+        contacts = new ArrayList<Person>();
     }
 
     public ArrayList<Person> getContacts()
@@ -27,6 +27,7 @@ public class ContactList {
         System.out.println("1: Person");
         System.out.println("2: Athlete");
         int hold = s.nextInt();
+        s.nextLine();
         if (hold == 1)
         {
             System.out.println("Please fill out the following information");
@@ -40,6 +41,7 @@ public class ContactList {
             grade = s.nextInt();
             Student a = new Student(firstName, lastName, phoneNumber, grade);
             contacts.add(a);
+
         }
         if (hold == 2)
         {
@@ -64,12 +66,172 @@ public class ContactList {
         }
     }
 
-    public void sort(int sortBy)
-    {
+    public void sort(int sortBy) {
+        Person p;
+        int number;
         if (sortBy == 0)
         {
-
+            int n = contacts.size();
+            for (int pass = 1; pass < n; pass++)
+            {
+                for (int comp = 0; comp < n - pass; comp++)
+                {
+                    if (contacts.get(comp).getFirstName().compareTo(contacts.get(comp + 1).getFirstName()) > 0)
+                    {
+                        p = contacts.get(comp);
+                        contacts.set(comp, contacts.get(comp + 1));
+                        contacts.set(comp + 1, p);
+                    }
+                }
+            }
+            printContacts();
+        }
+        else if (sortBy == 1)
+        {
+            int n = contacts.size();
+            for (int pass = 1; pass < n; pass++)
+            {
+                for (int comp = 0; comp < n - pass; comp++)
+                {
+                    if (contacts.get(comp).getLastName().compareTo(contacts.get(comp + 1).getLastName()) > 0)
+                    {
+                        p = contacts.get(comp);
+                        contacts.set(comp, contacts.get(comp + 1));
+                        contacts.set(comp + 1, p);
+                    }
+                }
+            }
+            printContacts();
+        }
+        else
+        {
+            int n = contacts.size();
+            for (int pass = 1; pass < n; pass++)
+            {
+                for (int comp = 0; comp < n - pass; comp++)
+                {
+                    if (contacts.get(comp).getPhoneNumber().compareTo(contacts.get(comp + 1).getPhoneNumber()) > 0)
+                    {
+                        p = contacts.get(comp);
+                        contacts.set(comp, contacts.get(comp + 1));
+                        contacts.set(comp + 1, p);
+                    }
+                }
+            }
+            printContacts();
         }
     }
+    public Person searchByFirstName(String firstName)
+    {
+        for (Person K: contacts)
+        {
+            if (K.getFirstName().equals(firstName))
+            {
+                return K;
+            }
+        }
+        return null;
+    }
+    public Person searchByLastName(String lastName)
+    {
+        for (Person K: contacts)
+        {
+            if (K.getLastName().equals(lastName))
+            {
+                return K;
+            }
+        }
+        return null;
+    }
+    public Person searchByPhoneNumber(String phoneNumber)
+    {
+        for (Person K: contacts)
+        {
+            if (K.getPhoneNumber().equals(phoneNumber))
+            {
+                return K;
+            }
+        }
+        return null;
+    }
+    public void listStudents()
+    {
+        for (Person contact: contacts)
+        {
+            if (contact instanceof Student)
+            {
+                System.out.println(contact);
+            }
+        }
+    }
+    private void printMenu()
+    {
+        System.out.println("Menu:");
+        System.out.println("1. Add Contact");
+        System.out.println("2. List All Contacts By First Name");
+        System.out.println("3. List All Contacts By Last Name");
+        System.out.println("4. List All Contacts By Phone Name");
+        System.out.println("5. List All Students");
+        System.out.println("6. Search By First Name");
+        System.out.println("7. Search By Last Name");
+        System.out.println("8. Search By Phone Number");
+        System.out.println("0. Exit");
 
+    }
+    public void Run()
+    {
+        int options;
+        String holder;
+        Scanner scanner = new Scanner(System.in);
+        do
+        {
+            printMenu();
+            options = scanner.nextInt();
+            scanner.nextLine();
+            {
+                {
+                    switch(options)
+                    {
+                        case 1:
+                            addContact();
+                            break;
+                        case 2:
+                            sort(0);
+                            break;
+                        case 3:
+                            sort(1);
+                            break;
+                        case 4:
+                            sort(2);
+                            break;
+                        case 5:
+                            listStudents();
+                            break;
+                        case 6:
+                            System.out.println("First Name: ");
+                            holder = scanner.nextLine();
+                            System.out.println(searchByFirstName(holder));
+                            break;
+                        case 7:
+                            System.out.println("Last Name:");
+                            holder = scanner.nextLine();
+                            System.out.println(searchByLastName(holder));
+                            break;
+                        case 8:
+                            System.out.println("Phone Number");
+                            holder = scanner.nextLine();
+                            System.out.println(searchByPhoneNumber(holder));
+                            break;
+                    }
+                }
+            }
+        }
+        while (options != 0);
+    }
+
+    public static void main(String[] args)
+    {
+        ContactList Kavan = new ContactList();
+        Kavan.Run();
+    }
 }
